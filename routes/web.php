985f648776c7d243d6801f96f4c1214cd8cd0e45
melmatx <melpalana13@gmail.com>
2023-07-24 00:10:19 +0800

    Added logo and changed initial route

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/browse', function () {
+    return view('browse');
+})->middleware(['auth', 'verified'])->name('browse');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
