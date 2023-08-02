54edeb4f8ea034b2d7c316591cc9b0b032e84c3e
melmatx <melpalana13@gmail.com>
2023-08-02 21:40:52 +0800

    Product card list visual fix

--- resources/views/components/product/card-list.blade.php
+++ resources/views/components/product/card-list.blade.php
@@ -1,9 +1,13 @@
 @props(['products'])
 
 <section class="mx-auto max-w-2xl px-4 pb-10 sm:px-6 lg:max-w-7xl lg:px-8">
-    <div class="py-8 text-black">
-        {{ $slot }}
-    </div>
+    @if($slot->isNotEmpty())
+        <div class="py-8 text-black">
+            {{ $slot }}
+        </div>
+    @else
+        <div class="py-4"></div>
+    @endif
 
     <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         @foreach($products as $product)
